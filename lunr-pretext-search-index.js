08bc1e55c7f115d738a04d33f61086ee47c631b6
dejojam1 <jamie.dejong@umanitoba.ca>
2026-07-09 02:04:55 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -169,12 +169,12 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "4",
   "title": "Testing",
-  "body": " Testing    Lists:  Item 1         Another item 1       A Table:  Just testing stuff    Heading 1  Heading 2  Heading 3  Heading 4  Heading 5    5  6  7  8          The UofM Logo   The University of Manitoba's logo.    "
+  "body": " Testing    Lists:  Item 1         Another item 1       A Table:   Just testing stuff    Heading 1  Heading 2  Heading 3  Heading 4  Heading 5    5  6  7  8         The UofM Logo  The UofM Logo   The University of Manitoba's logo.    "
 },
 {
-  "id": "ch-testing-4-1",
+  "id": "ch-testing-5",
   "level": "2",
-  "url": "ch-testing.html#ch-testing-4-1",
+  "url": "ch-testing.html#ch-testing-5",
   "type": "Table",
   "number": "4.0.1",
   "title": "Just testing stuff",
@@ -187,7 +187,7 @@ var ptx_lunr_docs = [
   "type": "Figure",
   "number": "4.0.2",
   "title": "The UofM Logo",
-  "body": " The UofM Logo   The University of Manitoba's logo.   "
+  "body": " The UofM Logo  The UofM Logo   The University of Manitoba's logo.   "
 }
 ]
 
